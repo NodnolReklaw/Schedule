8b37e86c6b6efc2b134c7b82b71c7a7fcf488d8e
London Walker <nodnolreklaw@gmail.com>
2018-07-18 10:00:08 -0600

    started library and schedule sections

---
+++ src/components/Schedule/schedule.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Schedule extends Component {
+    return() {
+        return (
+            <div className="schedule">
+                <h1 className="schedule__title">Course schedule</h1>
+            </div>
+        );
+    }
+}
+
+export default Schedule;
